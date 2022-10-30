ce6f330bea191b71edd93bd9b5776b3b70e83f65
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-10-30 19:05:44 -0400

    Update options.lua

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,1 +1,10 @@
+vim.opt.number = true
+vim.opt.encoding ="utf-8"
+vim.opt.numberwidth = 4
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
 
+vim.opt.scrolloff = 6
+vim.opt.wrap = false
